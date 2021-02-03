fb03c74b721961697d896103c1250eeee714f5dc
Kruimeldief <55532712+Kruimeldief@users.noreply.github.com>
2021-02-03 11:44:30 +0100

    First commit with exercise B2.

---
+++ week1/week1/main.c
@@ -0,0 +1,18 @@
+/*
+ * week1.c
+ *
+ * Created: 3-2-2021 11:41:38
+ * Author : Dennis
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
