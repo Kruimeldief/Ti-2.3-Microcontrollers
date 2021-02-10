6d8a6962042986956a11ee550f820b604ed2913b
Kruimeldief <55532712+Kruimeldief@users.noreply.github.com>
2021-02-10 10:51:05 +0100

    Commitity b2

---
+++ week1/week2/b2/b2.c
@@ -0,0 +1,34 @@
+#define F_CPU 8e6
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+uint8_t stepperLight = 0;
+
+ISR( INT0_vect ) {
+    stepperLight++;
+	PORTA = 1 << (stepperLight % 8);
+}
+
+ISR( INT1_vect ) {
+    stepperLight--;
+	PORTA = 1 << (stepperLight % 8);
+}
+
+int main( void ) {
+	// Init I/O
+	DDRA = 0b11111111;		// PORTA(0:7) output
+
+	// Init Interrupt hardware
+	EICRA |= 0x0B;			// INT1 falling edge, INT0 rising edge
+	EIMSK |= 0x03;			// Enable INT1 & INT0
+	
+	// Enable global interrupt system
+	//SREG = 0x80;			// Of direct via SREG of via wrapper
+	sei();				
+
+	while (1) {							
+	}
+
+	return 1;
+}
