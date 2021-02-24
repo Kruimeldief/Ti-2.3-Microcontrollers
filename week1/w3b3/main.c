7da24f83ca720095f621a35f86aa8d388959ab00
Kruimeldief <55532712+Kruimeldief@users.noreply.github.com>
2021-02-24 12:28:57 +0100

    W3B3

--- week1/w3b3/main.c
+++ week1/w3b3/main.c
@@ -11,42 +11,41 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include "../w2b5/lcd/lcd.h"
 
 
-// Build in led Arduino on PB7 (pin 13)
-void wait( int ms ) {
-	for (int i=0; i<ms; i++) {
-		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
-	}
-}
+int msHigh = 25;
+int msLow = 15;
+volatile int msCount = 0;
+volatile int ms = 15;
 
-volatile int tenthValue = 0;
-volatile int CompareValue = 0;
-
-ISR( TIMER2_COMP_vect ) {
-	TCNT2 = 0;
-	tenthValue++;
-}
-
-void setupTimer(void) {
-	cli();
-	OCR2 = CompareValue;
+void timer2Init( void ) {
+	OCR2 = 250;
 	TIMSK |= BIT(7);
 	sei();
-	TCCR2 = 0b00011111;
+	TCCR2 = 0b00001011;
+}
+
+ISR( TIMER2_COMP_vect ) {
+	msCount++;
+	if ( msCount == ms) {
+		PORTD ^= BIT(7);
+		msCount = 0;
+		if (ms == msHigh) {
+			ms = msLow;
+		} else {
+			ms = msHigh;
+		}
+	}
 }
 
 
 int main(void) {
-	DDRD &= ~BIT(7);
-	DDRA = 0xFF;
-	DDRB = 0xFF;
-	setupTimer();
+	DDRD = 0xFF;
+	timer2Init();
 	
     while (1)  {
-		PORTA = TCNT2;
-		PORTB = tenthValue;
-		wait(10);
+		_delay_ms(10);
     }
 	
 	return 0;
